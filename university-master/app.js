d17a1ddc81a7f58df9ea5346b8e0e9c6f8b8103a
unknown <kunsjeunh4n@gmail.com>
2020-04-11 15:08:46 +0700

    fix upload image2

--- university-master/app.js
+++ university-master/app.js
@@ -49,57 +49,62 @@ console.log('The magic happens on port ' + port);
 
 //upload file
 
-// const  multipart  =  require('connect-multiparty');
-// const  multipartMiddleware  =  multipart({ uploadDir:  './upload' });
-// app.get('/api/upload', (req, res) => {
-//     res.json({
-//         'message': 'hello'
-//     });
-// });
+const  multipart  =  require('connect-multiparty');
+const  multipartMiddleware  =  multipart({ uploadDir:  './upload' });
+app.get('/api/upload', (req, res) => {
+    res.json({
+        'message': 'hello'
+    });
+});
 // app.post('/api/upload', multipartMiddleware, (req, res) => {
 //     res.json({
 //         'message': 'File uploaded successfully'
 //     });
 // });
 
-app.get('/api/upload', (req, res) => {
-  // res.sendFile(path.join(`${__dirname}/src/app/components/form3/form3.component.html`));
-  res.json({
-        'message': 'hello'
-      });
-});
-
 let diskStorage = multer.diskStorage({
   destination: (req, file, callback) => {
-    // Định nghĩa nơi file upload sẽ được lưu lại
     callback(null, "./upload");
   },
   filename: (req, file, callback) => {
-    // ở đây các bạn có thể làm bất kỳ điều gì với cái file nhé.
-    // Mình ví dụ chỉ cho phép tải lên các loại ảnh png & jpg
     let math = ["image/png", "image/jpeg"];
     if (math.indexOf(file.mimetype) === -1) {
       let errorMess = `The file <strong>${file.originalname}</strong> is invalid. Only allowed to upload image jpeg or png.`;
       return callback(errorMess, null);
     }
-    // Tên của file thì mình nối thêm một cái nhãn thời gian để đảm bảo không bị trùng.
     let filename = `${Date.now()}-${file.originalname}`;
     callback(null, filename);
   }
 });
-
+// app.get('/api/upload', (req, res) => {
+//   // res.sendFile(path.join(`${__dirname}/src/app/components/form3/form3.component.html`));
+//   res.json({
+//         'message': 'hello'
+//       });
+// });
+//
+// let diskStorage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, "./upload");
+//   },
+//   filename: (req, file, callback) => {
+//     let math = ["image/png", "image/jpeg"];
+//     if (math.indexOf(file.mimetype) === -1) {
+//       let errorMess = `The file <strong>${file.originalname}</strong> is invalid. Only allowed to upload image jpeg or png.`;
+//       return callback(errorMess, null);
+//     }
+//     let filename = `${Date.now()}-${file.originalname}`;
+//     callback(null, filename);
+//   }
+// });
+//
 let uploadFile = multer({storage: diskStorage}).single("file");
-
+//
 app.post('/api/upload', (req, res) => {
-  // Thực hiện upload file, truyền vào 2 biến req và res
   uploadFile(req, res, (error) => {
-    // Nếu có lỗi thì trả về lỗi cho client.
-    // Ví dụ như upload một file không phải file ảnh theo như cấu hình của mình bên trên
     if (error) {
       return res.send(`Error when trying to upload: ${error}`);
     }
-    // Không có lỗi thì lại render cái file ảnh về cho client.
-    // Đồng thời file đã được lưu vào thư mục uploads
     res.sendFile(path.join(`${__dirname}/uploads/${req.file.filename}`));
   });
 });
